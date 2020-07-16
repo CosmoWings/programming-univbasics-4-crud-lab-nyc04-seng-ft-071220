6d9bd455a38e81246c47a887cb91dc42b930cf12
Patrick Pierre <patrick.pierre000@gmail.com>
2020-07-16 17:20:03 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -38,7 +38,9 @@ def retrieve_first_element_from_array(array)
 end
 
 def retrieve_last_element_from_array(array)
-  
+  last_element_of_array = array[-1]
+  last_element_of_array
+end
 end
 
 def update_element_from_index(array, index_number, element)
